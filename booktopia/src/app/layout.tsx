8c708ec6313455d9ba9b283596e9557cbdeede09
Zhile Lin <zhlin04@gmail.com>
2023-05-10 19:42:47 -0700

    set up message request

--- booktopia/src/app/layout.tsx
+++ booktopia/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Chat from '@/components/Chat'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
           <Chat/>
         {children}
+        </Providers>
         </body>
     </html>
   )
--- booktopia/src/components/ChatInput.tsx
+++ booktopia/src/components/ChatInput.tsx
@@ -4,20 +4,49 @@ import { FC,HTMLAttributes } from 'react'
 import { cn } from '@/lib/utils'
 import TextareaAutosize from 'react-textarea-autosize'
 import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { nanoid } from 'nanoid'
+import { Message } from '@/lib/validators/message'
 
-interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{
-    
-}
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{}
 
 const ChatInput: FC<ChatInputProps> = ({className,...props}) => {
 
  const[input,setInput] = useState<string>('')
+
+ const {mutate:sendMessage,isLoading} = useMutation({
+     mutationFn:async(message:Message)=>{
+       const response = await fetch('/api/message',{
+            method:'POST',
+            headers:{
+               'Content-Type':'application/json'
+            },
+            body:JSON.stringify({messages:"Hello"})
+       })
+
+       return response.body
+     },
+     onSuccess:()=>{
+         console.log('Success')
+     }
+ })
 
   return <div {...props} className={cn('border-t border-zinc-300',className)}>
       <div className='relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none'>
             <TextareaAutosize
                rows={2}
                maxRows={4}
+               onKeyDown={(e)=>{
+                  if(e.key === 'Enter' && !e.shiftKey){
+                     e.preventDefault()
+                     const message:Message = {
+                        id:nanoid(),
+                        isUserMessage:true,
+                        text:input
+                     }
+                    sendMessage(message)
+                  }
+               }}
                autoFocus
                value={input}
                onChange={(e)=>setInput(e.target.value)}
