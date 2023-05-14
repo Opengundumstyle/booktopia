5072bb286054583cef89dd477d69df675471d519
Zhile Lin <zhlin04@gmail.com>
2023-05-14 02:37:26 -0700

    style logo

--- booktopia/src/app/page.tsx
+++ booktopia/src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="absolute inset-0 flex justify-center items-center">
-         Bitches
+         <div className='flex flex-col items-center justify-center gap-3'>
+          <Image src='/booktopia-website-favicon-color.png' alt='logo' width={100} height={100}/>
+          <div> Booktopia Chat</div>
+          </div>
     </main>
   )
 }
