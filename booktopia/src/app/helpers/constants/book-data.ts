14862c2ff98cc83aa2c12b6879c51476cd1c8fa2
Zhile Lin <zhlin04@gmail.com>
2023-05-14 01:05:14 -0700

    loader

--- booktopia/src/app/helpers/constants/book-data.ts
+++ booktopia/src/app/helpers/constants/book-data.ts
@@ -1,41 +1,41 @@
 export const bookData = `
 <url>
-<loc>https://www.astorscientific.com/</loc>
-<desc>astor scientific store</desc>
+<loc>https://books.toscrape.com/</loc>
+<desc>Your book store</desc>
 </url>
 <url>
-<loc> https://www.astorscientific.com/</loc>
-<desc>An overview about our product collection</desc>
+<loc> https://books.toscrape.com/</loc>
+<desc>An overview about our book collection</desc>
 </url>
 <url>
-<loc>https://www.astorscientific.com/category/lab-supplies/</loc>
+<loc>https://books.toscrape.com/catalogue/the-artists-way-a-spiritual-path-to-higher-creativity_839/index.html</loc>
 <content>
     <book>
     <url>https://www.astorscientific.com/product/nest-cell-culture-plate-flat-tc-sterile-1-pk-6-384-wells/</url>
-    <desc>life science</desc>
-    <price>38.89</price>
+    <desc>"Without The Artist's Way, there would have been no Eat, Pray, Love.” —Elizabeth GilbertThe Artist’s Way is the seminal book on the subject of creativity. An international bestseller, millions of readers have found it to be an invaluable guide to living the artist’s life</desc>
+    <price>38.49</price>
     <availability>in stock</availability>
     </book>
     <book>
-    <url>https://www.astorscientific.com/product/nest-cell-culture-plate-flat-tc-sterile-1-pk-6-384-wells/</url>
-    <desc>lifescience</desc>
-    <price>49.43</price>
+    <url>https://books.toscrape.com/catalogue/the-power-of-now-a-guide-to-spiritual-enlightenment_855/index.html</url>
+    <desc>Ekhart Tolle's message is simple: living in the now is the truest path to happiness and enlightenment. And while this message may not seem stunningly original or fresh, Tolle's clear writing, supportive voice and enthusiasm make this an excellent manual for anyone who's ever wondered what exactly "living in the now" means.</desc>
+    <price>43.54</price>
     <availability>in stock</availability>
     </book>
 </content>
 </url>
 <url>
-<loc>https://www.astorscientific.com/category/medical-supplies/</loc>
+<loc>https://books.toscrape.com/catalogue/category/books/self-help_41/index.html</loc>
 <content>
     <book>
-    <url>https://books.toscrape.com/catalogue/how-to-stop-worrying-and-start-living_431/index.html</url>
-    <desc>life science </desc>
+    <url>https://books.toscrape.com/catalogue/overload-how-to-unplug-unwind-and-unleash-yourself-from-the-pressure-of-stress_725/index.html</url>
+    <desc>#1 New York Times bestselling author Joyce Meyer shows readers how to become free from the burden of stress so that they can achieve God's best for their lives. </desc>
     <price>46.49</price>
     <availability>in stock</availability>
     </book>
     <book>
-    <url>https://www.astorscientific.com/product/nest-cell-culture-plate-flat-tc-sterile-1-pk-6-384-wells/</url>
-    <desc>life science</desc>
+    <url>https://books.toscrape.com/catalogue/how-to-stop-worrying-and-start-living_431/index.html</url>
+    <desc>Learn how to break the worry habit -- Now and forever! With Dale Carnegie's timeless advice in hand, more than six million people have learned how to eliminate debilitating fear and worry from their lives and to embrace a worry-free future. </desc>
     <price>46.03</price>
     <availability>in stock</availability>
     </book>
--- booktopia/src/components/ChatInput.tsx
+++ booktopia/src/components/ChatInput.tsx
@@ -8,6 +8,7 @@ import { useMutation } from '@tanstack/react-query'
 import { nanoid } from 'nanoid'
 import { Message } from '@/lib/validators/message'
 import { MessagesContext } from '@/context/messages'
+import { CornerDownLeft, Loader2 } from 'lucide-react'
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{}
 
@@ -69,12 +70,14 @@ const ChatInput: FC<ChatInputProps> = ({className,...props}) => {
      }
  })
 
+
   return <div {...props} className={cn('border-t border-zinc-300',className)}>
       <div className='relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none'>
             <TextareaAutosize
                ref={textareaRef}
                rows={2}
                maxRows={4}
+               disabled={isLoading}
                onKeyDown={(e)=>{
                   if(e.key === 'Enter' && !e.shiftKey){
                      e.preventDefault()
@@ -90,12 +93,15 @@ const ChatInput: FC<ChatInputProps> = ({className,...props}) => {
                value={input}
                onChange={(e)=>setInput(e.target.value)}
                placeholder='Write a message..'
-               className='peer disabled:opacipty-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
+               className='peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
             />
 
-            <div className='absolute inset-y-0 flex py-1.5 pr-1.5'>
-                <kbd className='inline-flex items-center rounded border bg-white'></kbd>
+            <div className='absolute inset-y-0 right-0 flex py-1.5 pr-1.5'>
+                <kbd className='inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sans'>
+                    {isLoading?<Loader2 className='w-2 h-2 animate-spin' />:<CornerDownLeft className='w-3 h-3 '/>}
+                </kbd>
             </div>
+            <div aria-hidden='true' className='absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600'/>
       </div>
   </div>
 }
